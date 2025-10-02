32448f26ddeb5f7a077abe923f5461d1e9f76428
Jfscastro88 <joaofscastro@gmail.com>
2025-10-02 16:10:31 +0200

    favicon

--- print7/src/layout/Layout.jsx
+++ print7/src/layout/Layout.jsx
@@ -1,6 +1,5 @@
 import "@mantine/core/styles.css";
 import "../../src/index.css";
-import { Box } from "@mantine/core";
 import { Outlet } from "react-router-dom";
 import Navbar from "../assets/components/navbar.jsx";
 import Footer from "../assets/components/footer.jsx";
